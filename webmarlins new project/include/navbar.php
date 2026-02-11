53ac7c2de5fb066fc5ca61355149fb9d631f7d43
Aman7277684594 <144048189+Aman7277684594@users.noreply.github.com>
2026-02-11 16:02:13 +0530

    Add files via upload

---
+++ webmarlins new project/include/navbar.php
@@ -0,0 +1,12 @@
+
+        <nav aria-label="Main">
+          <ul>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#why">Why Us</a></li>
+            <li><a href="#cases">Case Studies</a></li>
+            <li><a href="#reviews">Reviews</a></li>
+            <li><a href="#process">Process</a></li>
+            <li><a href="#blog">Blog</a></li>
+            <li><a href="#faq">FAQ</a></li>
+          </ul>
+        </nav>
